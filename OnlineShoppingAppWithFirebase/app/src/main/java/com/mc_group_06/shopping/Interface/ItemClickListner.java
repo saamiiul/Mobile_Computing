d873f10d9b26d4aac48eceea3ff3c9253a938717
sami2905 <bsef18a028@pucit.edu.pk>
2021-07-10 10:56:20 +0500

    Online Shopping App With Firebase Database

---
+++ OnlineShoppingAppWithFirebase/app/src/main/java/com/mc_group_06/shopping/Interface/ItemClickListner.java
@@ -0,0 +1,7 @@
+package com.mc_group_06.shopping.Interface;
+import android.view.View;
+
+public interface ItemClickListner
+{
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ OnlineShoppingAppWithFirebase/app/src/main/java/com/mc_group_06/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.mc_group_06.shopping.Prevalent;
+
+import com.mc_group_06.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
